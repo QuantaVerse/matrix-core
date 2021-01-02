f232b79790b78b35991d3ef92c71c75bb8a5e361
Pritam Sarkar <001pritam2012@gmail.com>
2021-01-03 02:58:55 +0530

    fix: 🐛 Modifying release system

--- release.config.js
+++ release.config.js
@@ -2,12 +2,8 @@ module.exports = {
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        ["@semantic-release/npm", {
-            "tarballDir": "release"
-        }],
         ["@semantic-release/github", {
-            "assets": "release/*.tgz"
+            "assets": ["dist/**"]
         }],
         "@semantic-release/git"
     ],
